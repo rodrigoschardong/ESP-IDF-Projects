81cb34927a8e6bbd430a45bb38024cca49bded8b
Rodrigo Schardong de Almeida Rego <rscharong@gmail.com>
2021-03-05 09:56:25 -0300

    Update main.c

--- Esp32_blink/src/main.c
+++ Esp32_blink/src/main.c
@@ -9,16 +9,16 @@
 #define RED_LED_PIN GPIO_NUM_32
 #define GREEN_LED_PIN GPIO_NUM_33
 
-#define TIMER_INTR_SEL TIMER_INTR_LEVEL  /*!< Timer level interrupt */
-#define TIMER_GROUP    TIMER_GROUP_0     /*!< Test on timer group 0 */
-#define TIMER_DIVIDER   80               /*!< Hardware timer clock divider, 80 to get 1MHz clock to timer */
-#define TIMER_SCALE    (TIMER_BASE_CLK / TIMER_DIVIDER)  /*!< used to calculate counter value */
-#define TIMER_FINE_ADJ   (0*(TIMER_BASE_CLK / TIMER_DIVIDER)/1000000) /*!< used to compensate alarm value */
-#define TIMER_INTERVAL0_SEC   (1)//(0.001)   /*!< test interval for timer 0 */
+#define TIMER_INTR_SEL TIMER_INTR_LEVEL                                  // Timer level interrupt
+#define TIMER_GROUP    TIMER_GROUP_0                                     // Test on timer group 0 
+#define TIMER_DIVIDER   80                                               // Hardware timer clock divider, 80 to get 1MHz clock to timer
+#define TIMER_SCALE    (TIMER_BASE_CLK / TIMER_DIVIDER)                  // Used to calculate counter value 
+#define TIMER_FINE_ADJ   (0*(TIMER_BASE_CLK / TIMER_DIVIDER)/1000000)    // Used to compensate alarm value
+#define TIMER_INTERVAL0_SEC   (0.5)                                      // Test interval for timer 0 
 
 bool statusLed;
 
-void IRAM_ATTR timer_group0_isr(void *para){// timer group 0, ISR
+void IRAM_ATTR timer_group0_isr(void *para){    // Timer group 0, ISR
     int timer_idx = (int) para;
 
     statusLed = !statusLed;
@@ -41,25 +41,25 @@ static void tg0_timer0_init()
     config.divider = TIMER_DIVIDER;
     config.intr_type = TIMER_INTR_SEL;
     config.counter_en = TIMER_PAUSE;
-    /*Configure timer*/
+    
+    // Configure timer
     timer_init(timer_group, timer_idx, &config);
-    /*Stop timer counter*/
+    // Stop timer counter
     timer_pause(timer_group, timer_idx);
-    /*Load counter value */
+    // Load counter value
     timer_set_counter_value(timer_group, timer_idx, 0x00000000ULL);
-    /*Set alarm value*/
+    // Set alarm value
     timer_set_alarm_value(timer_group, timer_idx, (TIMER_INTERVAL0_SEC * TIMER_SCALE) - TIMER_FINE_ADJ);
-    /*Enable timer interrupt*/
+    // Enable timer interrupt
     timer_enable_intr(timer_group, timer_idx);
-    /*Set ISR handler*/
+    // Set ISR handler
     timer_isr_register(timer_group, timer_idx, timer_group0_isr, (void*) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
-    /*Start timer counter*/
+    // Start timer counter
     timer_start(timer_group, timer_idx);
 }
 
 void app_main(void){
     //setup
-
     statusLed = false;
 
     gpio_set_direction(RED_LED_PIN, GPIO_MODE_OUTPUT);
